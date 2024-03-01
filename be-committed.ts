16facd9cf41e97c98ed04df9438df75df164dcaf
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-01 17:00:34 -0500

    0.0.21

--- be-committed.ts
+++ be-committed.ts
@@ -2,7 +2,6 @@ import {BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
 import {BEConfig} from 'be-enhanced/types';
 import {XE} from 'xtal-element/XE.js';
 import {Actions, AllProps, AP, PAP, ProPAP, POA} from './types';
-import {register} from 'be-hive/register.js';
 
 export class BeCommitted extends BE<AP, Actions> implements Actions{
     static  override get beConfig(){
