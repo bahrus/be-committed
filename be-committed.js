9a02dbe068bb8e37a1b17ec68c3d598a09ba743d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-09-28 07:41:09 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-committed into baseline

--- be-committed.js
+++ be-committed.js
@@ -2,3 +2,40 @@
 import { BE } from 'be-enhanced/BE.js';
 import { propInfo, resolved, rejected } from 'be-enhanced/cc.js';
 
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AP, BAP, ObservingParameters} from './ts-refs/be-committed/types' */
+
+/**
+ * @implements {Actions}
+ * @implements {EventListenerObject}
+ */
+class BeCommitted extends BE {
+    /**
+     * @type {BEConfig<BAP, Actions & IEnhancement, any>}
+     */
+    static config = {
+        propInfo:{
+            ...propInfo
+        }
+    }
+
+    /**
+     * 
+     * @param {BAP} self 
+     */
+    hydrate(self){
+        return /** @type {PAP} */({
+        });
+    }
+
+    /**
+     * 
+     * @param {KeyboardEvent} e 
+     */
+    handleEvent(e){
+
+    }
+}
+
+await BeCommitted.bootUp();
+export {BeCommitted}
